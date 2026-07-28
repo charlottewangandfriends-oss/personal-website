d5c96bc3e772f2949404b20deb8d726219576264
charlottewangandfriends-oss <charlottewangandfriends@gmail.com>
2026-07-28 19:31:36 -0400

    Refine social card portrait position

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -53,6 +53,7 @@ export default async function OpenGraphImage() {
               height: '100%',
               objectFit: 'cover',
               objectPosition: '60% 28%',
+              transform: 'translateX(28px)',
             }}
           />
           <div
@@ -139,7 +140,7 @@ export default async function OpenGraphImage() {
               color: '#6a5a4d',
             }}
           >
-            transcending life with music and words
+            Transcending life with music and words
           </div>
         </div>
       </div>
